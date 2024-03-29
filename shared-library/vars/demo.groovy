77332fe47c43a3b2092066ed1cdc800edd38b49e
Deepthi <dpdevops6@gmail.com>
2024-03-29 14:19:49 -0400

    Jenkins

--- shared-library/vars/demo.groovy
+++ shared-library/vars/demo.groovy
@@ -5,3 +5,42 @@ def info(message) {
 def warning(message) {
     echo "WARNING: ${message}"
 }
+
+def call () {
+    pipeline {
+        agent any
+
+        stages {
+            stage('compile') {
+                steps {
+                    echo 'Hello World'
+                    script {
+                        info 'Starting'
+                        warning 'Nothing to do!'
+                    }
+                }
+            }
+            stage('test') {
+                steps {
+                    echo 'Hello World'
+                }
+            }
+            stage('code Quality') {
+                steps {
+                    echo 'Hello World'
+                }
+            }
+            stage('code Security') {
+                steps {
+                    echo 'Hello World'
+                }
+            }
+
+            stage('App Deploy') {
+                steps {
+                    echo 'Hello World'
+                }
+            }
+        }
+    }
+}
