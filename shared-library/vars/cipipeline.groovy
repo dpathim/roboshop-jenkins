ad388c97f3c5c175943f21ba9234aa0c506d39c5
Deepthi <dpdevops6@gmail.com>
2024-03-29 16:26:35 -0400

    Jenkins

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -1,10 +1,3 @@
-def info(message) {
-    echo "INFO: ${message}"
-}
-
-def warning(message) {
-    echo "WARNING: ${message}"
-}
 
 def call () {
     pipeline {
@@ -13,30 +6,30 @@ def call () {
         stages {
             stage('compile Code') {
                 steps {
-                    echo 'Hello World'
-                    script {
-                        info 'Starting'
-                        warning 'Nothing to do!'
-                    }
+                    sh 'env'
+
                 }
             }
-            stage('test') {
+            stage('Test') {
                 steps {
                     echo 'Hello World'
                 }
             }
-            stage('code Quality') {
+            stage('Code Quality') {
                 steps {
                     echo 'Hello World'
                 }
             }
-            stage('code Security') {
+            stage('Code Security') {
+                when {
+
+                }
                 steps {
                     echo 'Hello World'
                 }
             }
 
-            stage('App Deploy') {
+            stage('Release') {
                 steps {
                     echo 'Hello World'
                 }
